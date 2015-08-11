d0144394c90ed7af22dbce75a6bba5d4c7b80884
Almewty <almewty@gmail.com>
2015-08-11 18:33:18 +0200

    initial commit

---
+++ .tmp/app/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('riotApiChallenge2App', ['ngCookies', 'ngResource', 'ngSanitize', 'ui.router', 'ui.bootstrap']).config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $locationProvider.html5Mode(true);
+});
+//# sourceMappingURL=app.js.map
