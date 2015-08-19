bf2d6d706177406c6a4ab5ca2b472ce945ecc639
Almewty <almewty@gmail.com>
2015-08-19 20:33:30 +0200

    added filtering by patch

--- server/api/participant/participant.controller.js
+++ server/api/participant/participant.controller.js
@@ -14,37 +14,43 @@ exports.byItem = function (req, res) {
 	var offset = req.query.offset || 0;
 	var size = 500;
 	var itemId = Number(req.params.itemId);
+	var patch = req.params.patch;
 	if (isNaN(itemId))
 		return handleError(res, "Bad id");
 	Participant.find({
-		$or: [{
-				'stats.item0': itemId
+			$or: [{
+					'stats.item0': itemId
 		},
-			{
-				'stats.item1': itemId
+				{
+					'stats.item1': itemId
 			},
-			{
-				'stats.item2': itemId
+				{
+					'stats.item2': itemId
 			},
-			{
-				'stats.item3': itemId
+				{
+					'stats.item3': itemId
 			},
-			{
-				'stats.item4': itemId
+				{
+					'stats.item4': itemId
 			},
-			{
-				'stats.item5': itemId
+				{
+					'stats.item5': itemId
 			},
-			{
-				'stats.item6': itemId
+				{
+					'stats.item6': itemId
 			}]
 
-	}).sort({
-		_id: -1
-	}).exec(function (err, participants) {
-		if (err) return handleError(res, err);
-		return res.status(200).json(participants);
-	});
+		})
+		.populate('match')
+		.sort({
+			_id: -1
+		}).exec(function (err, participants) {
+			participants = _.filter(participants, function (p) {
+				return !patch || p.match.patch === patch;
+			});
+			if (err) return handleError(res, err);
+			return res.status(200).json(participants);
+		});
 };
 
 function handleError(res, err) {
