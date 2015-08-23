6c14db3bff2a1a807b49a013f1921b84d12c527d
raphaelwayne <raphael.reich1337@gmail.com>
2015-08-23 13:06:03 +0200

    Fill chart with db data

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,15 +1,18 @@
 'use strict';
 
 angular.module('riotApiChallenge2App')
-    .controller('MainCtrl', function ($scope, itemList) {
+    .controller('MainCtrl', function ($scope, $http, itemList) {
         $scope.items = [];
+        var championIds = [],
+            totalOld = [],
+            totalNew = [];
 
-        $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-        $scope.series = ['5.11.1', '5.14.1'];
+        $scope.labels = championIds;
+        $scope.series = ['5.11', '5.14'];
 
         $scope.data = [
-            [65, 59, 80, 81, 56, 55, 40],
-            [28, 48, 40, 19, 86, 27, 90]
+            totalOld,
+            totalNew
         ];
 
         if ($scope.items.length == 0) {
@@ -21,6 +24,22 @@ angular.module('riotApiChallenge2App')
         $scope.changeData = function (itemId) {
             $('.selected').removeClass('selected');
             $('.list-items img#item-' + itemId).addClass('selected');
-            console.log(itemId);
+            $http.get('/api/by-item/' + itemId).success(function (data) {
+                var statsOld = data['5.11'],
+                    statsNew = data['5.14'];
+                championIds.length = 0;
+
+                for (var i in statsOld) {
+                    if (i !== "total") {
+                        championIds.push(i);
+                        totalOld.push(statsOld[i].total);
+                    }
+                }
+                for (var i in statsNew) {
+                    if (i !== "total") {
+                        totalNew.push(statsNew[i].total);
+                    }
+                }
+            });
         };
     });
